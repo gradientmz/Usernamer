8221a2fb2e9e14462bfb77251703b5961557232c
gradientdev <xinyunzhouu@gmail.com>
2021-01-01 17:49:04 -0500

    Added ability to favorite usernames

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -8,17 +8,18 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = Set<WordPair>();
 
   Widget _buildList() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(14.0),
       itemBuilder: (context, item) {
         if (item.isOdd) return Divider();
 
         final index = item ~/ 2;
 
         if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(1));
+          _randomWordPairs.addAll(generateWordPairs().take(10));
         }
 
         return _buildRow(_randomWordPairs[index]);
@@ -27,8 +28,21 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+
     return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 19.0)));
+        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red[300] : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
   }
 
   Widget build(BuildContext context) {
