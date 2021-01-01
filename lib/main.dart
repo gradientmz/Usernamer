2e18dbe4e23036febc82af7026fc3a70f50ceae1
gradientdev <xinyunzhouu@gmail.com>
2021-01-01 15:45:46 -0500

    Second commit! Some minor widgets and english_wonds dependency

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: Scaffold (
+        appBar: AppBar(title: Text('usernamer')),
+        body: Center(
+          child: Text('Hello world')
+        )
+        ));
   }
 }
